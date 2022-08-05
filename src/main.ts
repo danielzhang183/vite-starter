aa9543672890afd88802d451a56a2765f3b130ae
danielzhang183 <danielzhang182@gmail.com>
2022-08-06 07:49:01 +0800

    feat: config router

--- src/main.ts
+++ src/main.ts
@@ -1,5 +1,13 @@
 import { createApp } from 'vue'
+import { createRouter, createWebHistory } from 'vue-router'
+import routes from 'virtual:generated-pages'
 import './style.css'
 import App from './App.vue'
 
-createApp(App).mount('#app')
+const app = createApp(App)
+const router = createRouter({
+  history: createWebHistory(import.meta.env.BASE_URL),
+  routes,
+})
+app.use(router)
+  .mount('#app')
---
+++ src/pages/index.vue
@@ -0,0 +1,12 @@
+<template>
+  <div>
+    <p>
+      <a rel="noreferrer" href="https://github.com/danielzhang183/vite-starter" target="_blank">
+        Vite Starter
+      </a>
+    </p>
+    <p>
+      <em>Opinionated Vite Starter Template</em>
+    </p>
+  </div>
+</template>
