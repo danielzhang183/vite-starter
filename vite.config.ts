288768e7849930a06a71954f539ea4b92a4eb70b
danielzhang183 <danielzhang182@gmail.com>
2022-08-07 08:44:00 +0800

    feat: auto import component

--- vite.config.ts
+++ vite.config.ts
@@ -2,6 +2,7 @@ import { defineConfig } from 'vite'
 import vue from '@vitejs/plugin-vue'
 import Pages from 'vite-plugin-pages'
 import Layouts from 'vite-plugin-vue-layouts'
+import Components from 'unplugin-vue-components/vite'
 import Unocss from 'unocss/vite'
 
 // https://vitejs.dev/config/
@@ -17,5 +18,10 @@ export default defineConfig({
 
     // https://github.com/antfu/unocss
     Unocss(),
+
+    // https://github.com/antfu/vite-plugin-components
+    Components({
+      dts: true,
+    }),
   ],
 })
