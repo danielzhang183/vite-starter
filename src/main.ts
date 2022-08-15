596bc989b54047f436aedc87f4a559a03058c3c5
danielzhang183 <danielzhang182@gmail.com>
2022-08-16 00:02:08 +0800

    feat: add Nprogress

--- src/main.ts
+++ src/main.ts
@@ -3,6 +3,7 @@ import { createRouter, createWebHistory } from 'vue-router'
 import { setupLayouts } from 'virtual:generated-layouts'
 import generatedRoutes from 'virtual:generated-pages'
 import { createPinia } from 'pinia'
+import NProgress from 'nprogress'
 import App from './App.vue'
 
 import '@unocss/reset/tailwind.css'
@@ -15,6 +16,12 @@ const router = createRouter({
   history: createWebHistory(import.meta.env.BASE_URL),
   routes: setupLayouts(generatedRoutes),
 })
+router.beforeEach((to, from) => {
+  if (to.path !== from.path)
+    NProgress.start()
+})
+router.afterEach(() => NProgress.done())
+
 app.use(router)
   .use(pinia)
   .mount('#app')
---
+++ src/modules/nprogress.ts
@@ -0,0 +1,11 @@
+import NProgress from 'nprogress'
+import type { Router } from 'vue-router'
+
+export const install = ({ router }: { router: Router }) => {
+  router.beforeEach((to, from) => {
+    if (to.path !== from.path)
+      NProgress.start()
+  })
+
+  router.afterEach(() => NProgress.done())
+}
